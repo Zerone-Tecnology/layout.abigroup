2e6096832e1750c5a57afd5d049160e55748bc63
Damir Rashidinov <damir.dmr88@gmail.com>
2024-05-17 19:57:38 +0500

    form, project, stages, reviews & partners

--- js/common.js
+++ js/common.js
@@ -4,11 +4,25 @@ $(function() {
 	  loop: true,
 		items: 1,
 	})
+
 	$('.slider-service').owlCarousel({
 		items: 1,
 	  loop: true,
 		nav: true,
-		// autoWidth: true,
+		margin: 15,
+	})
+	
+	$('.slider-project').owlCarousel({
+		items: 2,
+	  loop: true,
+		nav: true,
+		margin: 15,
+	})
+
+	$('.slider-reviews').owlCarousel({
+		items: 1,
+	  loop: true,
+		nav: true,
 		margin: 15,
 	})
 
