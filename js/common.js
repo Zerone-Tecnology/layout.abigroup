7179a217964a2cc24136f6c70af2698e9616079a
Damir Rashidinov <damir.dmr88@gmail.com>
2024-05-17 17:45:06 +0500

    prepos & service

--- js/common.js
+++ js/common.js
@@ -1,8 +1,15 @@
 $(function() {
 
-	$('.slider-wrap').owlCarousel({
+	$('.slider-main').owlCarousel({
 	  loop: true,
 		items: 1,
+	})
+	$('.slider-service').owlCarousel({
+		items: 1,
+	  loop: true,
+		nav: true,
+		// autoWidth: true,
+		margin: 15,
 	})
 
 });
