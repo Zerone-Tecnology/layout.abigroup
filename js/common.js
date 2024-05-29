7e6325a739302c8c970d5af55acf239e89eac117
Damir Rashidinov <damir.dmr88@gmail.com>
2024-05-29 12:03:24 +0500

    p certificates

--- js/common.js
+++ js/common.js
@@ -31,28 +31,35 @@ $(function() {
 	$('.slider-main').owlCarousel({
 	  loop: true,
 		items: 1,
-	})
+	});
 
 	$('.slider-service').owlCarousel({
 		items: 1,
 	  loop: true,
 		nav: true,
 		margin: 15,
-	})
+	});
 	
 	$('.slider-project').owlCarousel({
 		items: 2,
 	  loop: true,
 		nav: true,
 		margin: 15,
-	})
+	});
 
 	$('.slider-reviews').owlCarousel({
 		items: 1,
 	  loop: true,
 		nav: true,
 		margin: 15,
-	})
+	});
+
+	$('.slider-cert').owlCarousel({
+		items: 4,
+	  loop: true,
+		nav: true,
+		margin: 15,
+	});
 
 });
 
